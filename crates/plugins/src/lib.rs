d055da7afc27120ceca33febb65cc0465170c6da
Ethan Teng <1345118870@qq.com>
2022-10-24 20:50:49 +0800

    [feat] init core module

--- crates/plugins/src/lib.rs
+++ crates/plugins/src/lib.rs
@@ -1,18 +1,1 @@
-mod codegen;
-mod deps_inject;
-mod error_capture;
-mod get_config;
-mod thread_pool;
 
-pub struct FMT {
-    // TODO
-}
-
-impl FMT {
-    pub fn new() -> Self {
-        todo!()
-    }
-    pub fn format(&self) {
-        todo!()
-    }
-}
